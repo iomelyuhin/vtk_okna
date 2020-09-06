bf6cc7aded108784dd54f2e8a57de81a8167da76
Ilya Omelyukhin <i.omelyuhin@hmail.com>
2020-09-06 21:57:02 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,175 @@
+const { src, dest, task, series, watch, parallel } = require("gulp"); //подключение методов
+const rm = require("gulp-rm"); //for clean dist
+const sass = require("gulp-sass"); //for SASS
+const concat = require("gulp-concat"); //склейка файлов
+const browserSync = require("browser-sync").create(); //создаем сервер browser sync
+const reload = browserSync.reload; //автоматическая перезагрузка сервера
+const sassGlob = require("gulp-sass-glob"); //автоматическое добавление sass файлов
+// const autoprefixer = require('gulp-autoprefixer'); //автопрефиксер
+//const px2rem = require('gulp-smile-px2rem'); //пиксели в рем
+// const cleanCSS = require('gulp-clean-css'); //сжатие css
+// const sourcemaps = require('gulp-sourcemaps'); //запись sourcemaps
+// const babel = require('gulp-babel'); //переписывает код под любые заданные версии браузеров
+// const uglify = require('gulp-uglify'); //минифицирует код JS
+const gulpif = require("gulp-if"); //подключаем условие для prod и dev
+const cheerio = require("gulp-cheerio");
+const replace = require("gulp-replace");
+const svgstore = require("gulp-svgstore");
+const rename = require("gulp-rename");
+
+const env = process.env.NODE_ENV;
+
+sass.compiler = require("node-sass"); //for SASS
+
+task("clean", () => {
+  //for clean dist
+
+  return src("dist/**/*", { read: false }).pipe(rm());
+});
+
+task("copy:html", () => {
+  //копируем html
+  return src("src/*.html") //откуда
+    .pipe(dest("dist")) //куда
+    .pipe(reload({ stream: true })); //синхронизация изменений
+});
+
+task("copy:fonts", () => {
+  return src("src/fonts/*.*")
+    .pipe(dest("dist/fonts/"))
+    .pipe(reload({ stream: true }));
+});
+
+task("copy:img", () => {
+  return src("src/img/**/*.*")
+    .pipe(dest("dist/img/"))
+    .pipe(reload({ stream: true }));
+});
+
+task("copy:svg", () => {
+  return src("src/img/icons/*.svg")
+    .pipe(
+      cheerio({
+        run: function ($) {
+          $("[fill]").removeAttr("fill");
+          $("[stroke]").removeAttr("stroke");
+          $("[style]").removeAttr("style");
+        },
+        parserOptions: { xmlMode: true },
+      })
+    )
+		.pipe(replace("&gt;", ">"))
+		.pipe(
+      svgstore({
+        inlineSvg: true,
+      })
+    )
+    .pipe(rename("sprite.svg"))
+		.pipe(dest("dist/img/"))
+    .pipe(src("src/img/icons/*.svg"))
+		.pipe(dest("dist/img/icons/"))
+    .pipe(reload({ stream: true }));
+});
+
+task("copy:js-libs", () => {
+  return src("src/js/libs/*.js")
+    .pipe(dest("dist/js/libs/"))
+    .pipe(reload({ stream: true }));
+});
+
+const styles = [
+  //создаем переменную со стилями
+  // 'node_modules/normalize.css/normalize.css',
+  "src/css/main.scss",
+];
+
+task("styles", () => {
+  //for SASS
+  return (
+    src(styles) //вставляем переменную со стилями
+      // .pipe(gulpif(env === 'dev', sourcemaps.init())) //инициализируем SourceMap
+      .pipe(concat("main.scss")) //склеиваем в один файл
+      .pipe(sassGlob()) //автоматическое добавление sass файлов
+      .pipe(sass().on("error", sass.logError)) //обрабатываем sass
+      //.pipe(px2rem()) //px to rem
+      // .pipe(gulpif(env === 'dev', autoprefixer({ //автопрефиксер
+      //   browsers: ['last 2 versions'],
+      //   cascade: false
+      // })))
+      // .pipe(gulpif(env === 'prod', cleanCSS())) //сжимаем CSS
+      // .pipe(gulpif(env === 'dev', sourcemaps.write())) //записываем SourceMap
+      .pipe(dest("dist/css")) // складываем в готовую папку с проектом
+      .pipe(reload({ stream: true }))
+  );
+});
+
+const libs = [
+  //создаем массив с библиотеками
+  // "node_modules/jquery/dist/jquery.js",
+  // "src/js/libs/*.js",
+  "src/js/*.js",
+];
+
+task("scripts", () => {
+  return (
+    src(libs) //включаем все библиотеки в обработку
+      // .pipe((env === 'dev', sourcemaps.init())) //инициализируем SourceMap
+      // .pipe(concat('main.js', { newLine: ";" })) //склеиваем в один файл
+      // .pipe(babel({ //запускаем babel
+      //   presets: ['@babel/env']
+      // }))
+      // .pipe(gulpif(env === 'prod', uglify())) //минифицируем JS
+      // .pipe(gulpif(env === 'dev', sourcemaps.write())) //записываем SourceMap
+      .pipe(dest("dist/js")) // складываем в готовую папку с проектом
+      .pipe(reload({ stream: true }))
+  );
+});
+
+task("server", () => {
+  //разворачиваем сервер
+  browserSync.init({
+    server: {
+      baseDir: "./dist", //папка, из которой сервер возьмет файлы
+    },
+    open: false,
+  });
+});
+
+task("watch", () => {
+  watch("./src/css/**/*.scss", series("styles")); //следим за изменениями в sass-файлах
+  watch("./src/*.html", series("copy:html")); //следим за изменениями в html
+  watch("./src/img/**/*.*", series("copy:img")); //следим за изменениями в html
+  watch("./src/js/**/*.js", series("scripts")); //следим за изменениями в JS
+});
+
+task(
+  "default",
+  series(
+    "clean",
+    parallel(
+      "copy:html",
+      "copy:fonts",
+      "copy:img",
+			"copy:svg",
+			"copy:js-libs",
+      "styles",
+      "scripts"
+    ),
+    parallel("watch", "server")
+  )
+); //запуск серии тасков
+
+task(
+  "build",
+  series(
+    "clean",
+    parallel(
+      "copy:html",
+      "copy:fonts",
+      "copy:img",
+      "copy:svg",
+      "styles",
+      "scripts"
+    )
+  )
+); //запуск серии тасков
---
+++ src/js/slider.js
@@ -0,0 +1,212 @@
+document.addEventListener(`DOMContentLoaded`, function () {
+  function slider_run(js_slider, js_sliderList, js_sliderTrack, js_slides, js_arrows) {
+    let slider = document.querySelector(js_slider),
+      sliderList = slider.querySelector(js_sliderList),
+      sliderTrack = slider.querySelector(js_sliderTrack),
+      // sliderText = slider.querySelector(".js__slider-index").innerHTML,
+      slides = slider.querySelectorAll(js_slides),
+      arrows = slider.querySelector(js_arrows),
+      prev = arrows.children[0],
+      indexSlideText = arrows.children[1],
+      next = arrows.children[2],
+      slideWidth = slides[0].offsetWidth,
+      slideIndex = 0,
+      posInit = 0,
+      posX1 = 0,
+      posX2 = 0,
+      posY1 = 0,
+      posY2 = 0,
+      posFinal = 0,
+      isSwipe = false,
+      isScroll = false,
+      allowSwipe = true,
+      transition = true,
+      nextTrf = 0,
+      prevTrf = 0,
+      lastTrf = --slides.length * slideWidth,
+      posThreshold = slides[0].offsetWidth * 0.35,
+      trfRegExp = /([-0-9.]+(?=px))/,
+      getEvent = function () {
+        return event.type.search("touch") !== -1 ? event.touches[0] : event;
+      },
+      slide = function () {
+        if (transition) {
+          sliderTrack.style.transition = "transform .5s";
+        }
+        sliderTrack.style.transform = `translate3d(-${
+          slideIndex * slideWidth
+        }px, 0px, 0px)`;
+
+        prev.classList.toggle("disabled", slideIndex === 0);
+        next.classList.toggle("disabled", slideIndex === slides.length - 1);
+      },
+      swipeStart = function () {
+        let evt = getEvent();
+
+        if (allowSwipe) {
+          transition = true;
+
+          nextTrf = (slideIndex + 1) * -slideWidth;
+          prevTrf = (slideIndex - 1) * -slideWidth;
+
+          posInit = posX1 = evt.clientX;
+          posY1 = evt.clientY;
+
+          sliderTrack.style.transition = "";
+
+          document.addEventListener("touchmove", swipeAction);
+          document.addEventListener("mousemove", swipeAction);
+          document.addEventListener("touchend", swipeEnd);
+          document.addEventListener("mouseup", swipeEnd);
+
+          sliderList.classList.remove("grab");
+          sliderList.classList.add("grabbing");
+        }
+      },
+      swipeAction = function () {
+        let evt = getEvent(),
+          style = sliderTrack.style.transform,
+          transform = +style.match(trfRegExp)[0];
+
+        posX2 = posX1 - evt.clientX;
+        posX1 = evt.clientX;
+
+        posY2 = posY1 - evt.clientY;
+        posY1 = evt.clientY;
+
+        // определение действия свайп или скролл
+        if (!isSwipe && !isScroll) {
+          let posY = Math.abs(posY2);
+          if (posY > 7 || posX2 === 0) {
+            isScroll = true;
+            allowSwipe = false;
+          } else if (posY < 7) {
+            isSwipe = true;
+          }
+        }
+
+        if (isSwipe) {
+          // запрет ухода влево на первом слайде
+          if (slideIndex === 0) {
+            if (posInit < posX1) {
+              setTransform(transform, 0);
+              return;
+            } else {
+              allowSwipe = true;
+            }
+          }
+
+          // запрет ухода вправо на последнем слайде
+          if (slideIndex === --slides.length) {
+            if (posInit > posX1) {
+              setTransform(transform, lastTrf);
+              return;
+            } else {
+              allowSwipe = true;
+            }
+          }
+
+          // запрет протаскивания дальше одного слайда
+          if (
+            (posInit > posX1 && transform < nextTrf) ||
+            (posInit < posX1 && transform > prevTrf)
+          ) {
+            reachEdge();
+            return;
+          }
+
+          // двигаем слайд
+          sliderTrack.style.transform = `translate3d(${
+            transform - posX2
+          }px, 0px, 0px)`;
+        }
+      },
+      swipeEnd = function () {
+        posFinal = posInit - posX1;
+
+        isScroll = false;
+        isSwipe = false;
+
+        document.removeEventListener("touchmove", swipeAction);
+        document.removeEventListener("mousemove", swipeAction);
+        document.removeEventListener("touchend", swipeEnd);
+        document.removeEventListener("mouseup", swipeEnd);
+
+        sliderList.classList.add("grab");
+        sliderList.classList.remove("grabbing");
+
+        if (allowSwipe) {
+          if (Math.abs(posFinal) > posThreshold) {
+            if (posInit < posX1) {
+              slideIndex--;
+            } else if (posInit > posX1) {
+              slideIndex++;
+            }
+          }
+
+          if (posInit !== posX1) {
+            allowSwipe = false;
+            slide();
+          } else {
+            allowSwipe = true;
+          }
+        } else {
+          allowSwipe = true;
+        }
+      },
+      setTransform = function (transform, comapreTransform) {
+        if (transform >= comapreTransform) {
+          if (transform > comapreTransform) {
+            sliderTrack.style.transform = `translate3d(${comapreTransform}px, 0px, 0px)`;
+          }
+        }
+        allowSwipe = false;
+      },
+      reachEdge = function () {
+        transition = false;
+        swipeEnd();
+        allowSwipe = true;
+      };
+
+    sliderTrack.style.transform = "translate3d(0px, 0px, 0px)";
+    sliderList.classList.add("grab");
+
+    sliderTrack.addEventListener("transitionend", () => {
+      allowSwipe = true;
+      indexSlideText.innerHTML = `${slideIndex + 1}/${slides.length}`;
+    });
+    slider.addEventListener("touchstart", swipeStart);
+    slider.addEventListener("mousedown", swipeStart);
+
+    arrows.addEventListener("click", function () {
+      let target = event.target;
+
+      if (target.classList.contains("next")) {
+        slideIndex++;
+      } else if (target.classList.contains("prev")) {
+        slideIndex--;
+      } else {
+        return;
+      }
+
+      slide();
+      indexSlideText.innerHTML = `${slideIndex + 1}/${slides.length}`;
+    });
+    indexSlideText.innerHTML = `${slideIndex + 1}/${slides.length}`;
+  }
+
+  slider_run(
+    ".js__slider",
+    ".js__slider-list",
+    ".js__slider-track",
+    ".js__slide",
+    ".js__slider-arrows"
+  );
+  slider_run(
+    ".js__cases-slider",
+    ".js__cases-slider-list",
+    ".js__cases-slider-track",
+    ".js__cases-slide",
+    ".js__cases-slider-arrows"
+  );
+});
